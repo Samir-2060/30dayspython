d49bd2102e1caecab97863cd32062b8a006423b0
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 10:12:59 +0545

    Create if-else-statement.py

---
+++ day2/if-else-statement.py
@@ -0,0 +1,14 @@
+good_paper=True
+talent_boi=False
+
+if good_paper and talent_boi:
+    print("Will pass the exam")
+else:
+    print("padera kaam xaina khaadi jane ho")
+
+
+#Program 2
+not_handsome=False
+not_rich_kid=True
+if not_handsome or not_rich_kid:
+    print("You will be single")
