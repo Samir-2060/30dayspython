82d4adc852d6776226592efdcbe7de8adff43922
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 17:19:38 +0545

    Create list.py

---
+++ day2/list.py
@@ -0,0 +1,29 @@
+#program1
+name=["nirmal","samir","dhakal","prakash","aakash"]
+print(name)
+print(type(name))
+
+#program2
+
+name=["nirmal","samir","dhakal","prakash","aakash"]
+print(name[3])
+ 
+  
+ #program3
+name=["nirmal","samir","dhakal","prakash","aakash"]
+print(name[-1])
+print(name[-2])
+ 
+  #program4
+name=["nirmal","samir","dhakal","prakash","aakash"]
+print(name[2:])
+
+#program5
+name=["nirmal","samir","dhakal","prakash","aakash"]
+print(name[2:4])
+  
+  #program6
+name=["nirmal","samir","dhakal","prakash","aakash"]
+name[0]="Bishal"
+print(name)
+  
