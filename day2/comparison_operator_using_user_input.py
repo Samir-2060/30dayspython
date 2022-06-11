3f8168b5d73170c7bba10f964bbf45cf5320c373
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 10:23:11 +0545

    Create comparison_operator_using_user_input.py

---
+++ day2/comparison_operator_using_user_input.py
@@ -0,0 +1,7 @@
+temperature=float(input("Enter the temperature"))
+if temperature>30:
+    print("Its hot day")
+elif temperature<10:
+    print("Its cold day")
+else:
+    print("Its neither cold nor hot")
