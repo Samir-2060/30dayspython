9031865c789cbbef0ac4e0fa9fa2ac5ec9709356
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 09:03:16 +0545

    Create stringmethod.py

---
+++ day2/stringmethod.py
@@ -0,0 +1,12 @@
+user="Nirmal is a coder."
+print(user.find('is')) #is : because i is in index 7
+print(user.replace("coder","pro coder"))
+print("coder" in user)
+if 'coder' in user:
+    {
+        print("Yes found")
+    }
+else:
+    {
+        print("not found")
+    }
