f82f908b24e40f3a20517af28cf3143770d9c140
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 19:21:21 +0545

    Create listmethod.py

---
+++ day2/listmethod.py
@@ -0,0 +1,49 @@
+number=[20,40,3,50,4]
+number.append(60)
+print(number)
+
+#program2
+number=[20,40,3,50,4]
+print(number[2])
+number.insert(2,30)
+print(number)
+print(number[2])
+
+#program3
+number=[20,40,3,50,4]
+number.remove(40)
+print(number)
+
+#program4
+number=[20,40,3,50,4]
+number.remove(40)
+print(number)
+
+if 4 in number:
+    print("yes")
+else:
+    print("no")
+    
+    #program5
+number=[20,40,3,50,4]
+number.clear()
+print(number)
+
+#program6
+number=[20,40,3,50,4]
+number.pop()
+print(number)
+
+#program7
+number=[20,40,3,50,4]
+print(number.index(3))
+
+#program8
+number=[20,40,3,50,3,4]
+print(number.count(3))
+
+#program9
+number=[20,40,3,50,3,4]
+number.sort()
+print(number)
+ 
