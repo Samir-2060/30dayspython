301c2bfd65a999a82548787c76484dfe26ff3e1a
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 17:34:43 +0545

    Create 2dlist.py

---
+++ day2/2dlist.py
@@ -0,0 +1,26 @@
+matrix=[
+    [2,3,4],
+    [5,6,2],
+    [1,4,10]
+]
+print(matrix[1][1])
+
+#program2
+matrix=[
+    [2,3,4],
+    [5,6,2],
+    [1,4,10]
+]
+matrix[1][1]=7
+print(matrix[1][1])
+
+#program3
+matrix=[
+    [2,3,4],
+    [5,6,2],
+    [1,4,10]
+]
+for row in matrix:
+    for item in row:
+        print(item)
+ 
