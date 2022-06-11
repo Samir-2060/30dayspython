16c03471f33dcd6e5b83a82289c529aabbe476a9
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-11 10:49:08 +0545

    Update weight-converter.py

--- day2/weight-converter.py
+++ day2/weight-converter.py
@@ -8,3 +8,16 @@ elif unit=="K" or unit=="k":
     print(f"You are {unit2} Kg")
 else:
     print("something went wrong in very wrong way")
+
+    
+#program 2
+weight=float(input("Enter the weight"))
+unit=input("want to convert in (L)bs or (K)g:")
+if unit.upper()=="L":
+    unit2= weight *2.205
+    print(f"you are {unit2} pound")
+elif unit.upper()=="K":
+    unit2=weight*0.4535
+    print(f"You are {unit2} Kg")
+else:
+    print("something went wrong in very wrong way")
