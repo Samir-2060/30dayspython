f50972f219711a8836187a6d3d6359017bb6f4bd
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-12 10:59:08 +0545

    Update dictionaries.py

--- day3/dictionaries.py
+++ day3/dictionaries.py
@@ -44,5 +44,24 @@ digit_mapping={
 }
 output=""
 for ch in phone:
+    output +=digit_mapping[ch] + " "
+print(output)
+
+#program 4
+phone=input("Phone number:")
+digit_mapping={
+   "0":"zero",
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6": "six",
+    "7":"seven",
+    "8":"eight",
+    "9":"nine"
+}
+output=""
+for ch in phone:
     output +=digit_mapping.get(ch,"!") + " "
 print(output)
