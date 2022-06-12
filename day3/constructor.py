e0bcf7ca60176d9ffa2629c9aa4eb9dfedcf1e6f
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-12 13:08:52 +0545

    Create constructor.py

---
+++ day3/constructor.py
@@ -0,0 +1,29 @@
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def draw(self):
+        print("draw")
+    def move(self):
+        print("move")
+point1=Point(10,15)
+print(point1.x)
+print(point1.y)
+
+#program 2
+class Person:
+    def talk(self):
+        print("talk")
+bishal=Person()
+bishal.talk()
+
+#program3
+class Person:
+    def __init__(self,name):
+        self.name=name
+
+    def talk(self):
+        print("talk")
+name2=Person("samir")
+print(name2.name)
+name2.talk()
