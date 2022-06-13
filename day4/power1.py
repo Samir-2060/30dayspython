03b0b39a448775bf41b29dde19932eb1fb9c5712
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-13 17:45:59 +0545

    Create power1.py

---
+++ day4/power1.py
@@ -0,0 +1,8 @@
+
+#program2
+number1 =int(input("Enter the first number"))
+number2 =int(input("Enter the second number"))
+def power(number1,number2):
+result = number1**number2
+print(result)
+power(number1,number2)
