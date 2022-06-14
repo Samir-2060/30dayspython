22ce4516a88b12aad81663cce0f64ad6aa396490
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:26:11 +0545

    Create clear-method.py

---
+++ day5/clear-method.py
@@ -0,0 +1,8 @@
+#we use clear() method in list to clear whole element of list.In this even after clear() the list exist but the element of list no longer exist.Swah !!
+#program1
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.clear()
+
+print(fruits)
