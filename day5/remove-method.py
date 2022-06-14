fe9bba187912a7d3d2bc12f2fc15a51c47c3db3d
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:41:39 +0545

    Create remove-method.py

---
+++ day5/remove-method.py
@@ -0,0 +1,9 @@
+#used to remove item with specified value
+#Program1
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.remove("banana")
+
+print(fruits)
+
+#this program will remove the banana list element from the list.
