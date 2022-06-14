bd956b1c22d5d6a1743a441113d930fc115f14d2
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:39:14 +0545

    Create list-method.py

---
+++ day5/list-method.py
@@ -0,0 +1,8 @@
+#pop method is used to delete or remove the element of specified position using index number.
+#by default it return last item.
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.pop(1)
+
+print(fruits)
+
