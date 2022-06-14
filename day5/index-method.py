3cb6417ed3909370b1cc0ad3fe1f1d4efab31636
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:33:49 +0545

    Create index-method.py

---
+++ day5/index-method.py
@@ -0,0 +1,16 @@
+
+#index method is used to find the position of element of list.It describe where the element is located in the list.Index starts from 0.
+#program1
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+
+print(x)
+
+
+#program2
+fruits = [4, 55, 64, 32, 16, 32]
+
+x = fruits.index(32)
+
+print(x)
