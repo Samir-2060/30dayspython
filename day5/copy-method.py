db7d592fef6b066065fedced9775ed80abcf2ba4
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:27:17 +0545

    Create copy-method.py

---
+++ day5/copy-method.py
@@ -0,0 +1,6 @@
+#we use copy() method to duplicate one list in another list.The copy() method returns a copy of the specified list.
+fruits = ["apple", "banana", "cherry"]
+
+x = fruits.copy()
+
+print(x)
