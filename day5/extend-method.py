5e8cf9a3e3f21b36fa9848784723c74a3a1b2a40
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:32:06 +0545

    Create extend-method.py

---
+++ day5/extend-method.py
@@ -0,0 +1,11 @@
+#program1
+#extend method is ued to add element of one list to another list.
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+
+print(fruits)
+
+#here in above program cars list is added to the fruits list after completion of element of fruits element.
