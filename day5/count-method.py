0c06d5070ab83a6a5626bd50ace5f7134c45b2a8
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:29:35 +0545

    Update count-method.py

--- day5/count-method.py
+++ day5/count-method.py
@@ -1,6 +1,14 @@
 #count method return how many times the specified element of list repeated/appeared in the list
+#program1
 fruits = ["apple", "banana", "cherry"]
 
 x = fruits.count("cherry")
+
+print(x)
+
+#program2
+fruits = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+
+x = fruits.count(9)
 
 print(x)
