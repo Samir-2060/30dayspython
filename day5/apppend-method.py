b51db30b2c4ae2c651050da77dbbd1e876f0defe
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-14 20:23:52 +0545

    Create apppend-method.py

---
+++ day5/apppend-method.py
@@ -0,0 +1,18 @@
+#program1
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("orange")
+
+print(fruits)
+
+# add element to the end of the list 
+# we can also append two separat list using append method
+
+#program2
+a = ["apple", "banana", "cherry"]
+
+b = ["Ford", "BMW", "Volvo"]
+
+a.append(b)
+
+print(a)
