8a215d3ca915c941318a22cf6cbfdbd0c1b84603
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-23 14:14:48 +0545

    Update addstring.py

--- day11/addstring.py
+++ day11/addstring.py
@@ -8,5 +8,24 @@ elif len(string)<3:
     print(string)
 else:
     print(string + "ing")
+    
+    
+#program2
+#Write a Python program to add 'ing' at the end of a given string (length should
+#be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the
+#string length of the given string is less than 3, leave it unchanged.
+string=input("Enter the first string:")
+if len(string)>2:
+    if string[-3:]=="ing":
+        string+='ly'
+        print(string)
+    elif string[-3:]!="ing":
+        string+="ing"
+        print(string)
+    else:
+        print(string)
+
+
+
 
 
