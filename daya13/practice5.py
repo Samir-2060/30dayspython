9c89fe4de6b393a9f9c4f724b25c7a9714242f4e
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-25 14:43:36 +0545

    Update practice5.py

--- daya13/practice5.py
+++ daya13/practice5.py
@@ -11,3 +11,19 @@ def occurrences(string):
             count[word]=1
     return count
 print(occurrences("Hello I am live on youtube I am streaming now"))
+
+#program2
+#Write a Python program to count the occurrences of each word in a given sentence
+
+def count_string(string):
+    counts=dict()
+    words=string.split()
+
+    for word in words:
+        if word in counts:
+            counts[word]+=1
+        else:
+            counts[word]=1
+    return counts
+print(count_string("hello my name is my name"))
+
