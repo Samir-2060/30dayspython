704295a5a61a4d8563316025d99b0c8b99a64788
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-26 22:06:10 +0545

    Create day14.py

---
+++ day14.py
@@ -0,0 +1,14 @@
+#Write a Python script that takes input from the user and displays that input
+#back in upper and lower cases.
+
+user_input = input("What's your favorite fruit? ")
+print("My favourite language is ", user_input.upper())
+print("My favourite language is ", user_input.lower())
+
+
+#program2:
+user_input = input("What's your favorite fruit? ")
+upper=user_input.upper()
+lower=user_input.lower()
+print(upper)
+print(lower)
