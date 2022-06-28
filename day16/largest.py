1aa419fca66527952f230b021ff362016fba6472
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-06-28 15:08:56 +0545

    Update largest.py

--- day16/largest.py
+++ day16/largest.py
@@ -7,3 +7,19 @@ def largest_number(number):
              largest=i
     return largest
 print(largest_number([6,1,2,3,4,50,40,5]))
+
+#program2
+#Write a Python program to get the largest number from a list.
+
+def largest_number(number):
+    number.sort()
+    return number[-1]
+
+print(largest_number([1,2,3,50,34,40]))
+
+#program3
+def largest_number(number):
+     result=max(number)
+     return result
+
+print(largest_number([1,2,3,50,34,40]))
