19eb6ef6a1a46907f556d0271ecc64ab8333b681
itsmesamir <62460149+Samir-2060@users.noreply.github.com>
2022-07-01 11:20:22 +0545

    Create check.py

---
+++ day17/check.py
@@ -0,0 +1,8 @@
+#Write a Python program to check a list is empty or not.
+
+def check(lst):
+    if lst==[]:
+        print("yes the list is empty")
+    else:
+        print("No this is non empty list")
+(check([]))
